a675d4c843753a6182fe28333325f506f6a70861
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-12 21:33:56 -0500

    Print Line Added

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello! My name is Sadjell Mamon. What is your name?");
+		System.out.println("My name is Vanessa Smith! Nice to meet you!");
 
 	}
 
