faa06ff28291b9ce1f3c1cff3e3f56643df69ae5
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-12 22:37:19 -0500

    Changes made to new class

---
+++ src/Test2.java
@@ -0,0 +1,11 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is another class for the second part of the assignment");
+		System.out.println("I want to be creative, but I don't know what to say! Help!");
+
+	}
+
+}
