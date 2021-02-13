24845c1461bc57f0927c1eb44cf82e650440c449
Sadjell <57645947+Sadjell@users.noreply.github.com>
2021-02-12 21:53:49 -0500

    New Print Line Added

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Hello! My name is Sadjell Mamon. What is your name?");
 		System.out.println("My name is Vanessa Smith! Nice to meet you!");
 		System.out.println("Nice to meet you! Are you a freshman at QU?");
+		System.out.println("I just transfered to QU this year. I'm a sophomore.");
 		
 
 	}
